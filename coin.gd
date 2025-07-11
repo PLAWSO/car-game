736034b772a404d2fcf397fcb7bb3519f4860fc1
PLAWSO <lawson.payton76@gmail.com>
2025-07-10 19:23:12 -0500

    finished lesson 24. ignoring assets folder, hopefully?

--- coin.gd
+++ coin.gd
@@ -1,6 +1,8 @@
 extends Area3D
 
 const ROT_SPEED = 1 # number of degrees the coin rotates every frame
+@export var x : int
+@export var hud : CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +13,13 @@ func _process(delta: float) -> void:
 	rotate_y(deg_to_rad(ROT_SPEED))
 
 func _on_body_entered(body: Node3D) -> void:
+	Global.coins += 1
+	hud.get_node("CoinsLabel").text = str(Global.coins)
+	if Global.coins >= Global.NUM_COINS_TO_WIN:
+		get_tree().change_scene_to_file("res://win_screen.tscn")
+	set_collision_layer_value(3, false)
+	set_collision_mask_value(1, false)
+	$AnimationPlayer.play("bounce")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	queue_free()
--- steve.gd
+++ steve.gd
@@ -2,10 +2,28 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 12
 
+var xform : Transform3D
 
 func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	# Play Robot Animations:
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		$AnimationPlayer.play("jump")
+	elif is_on_floor() and input_dir != Vector2.ZERO:
+		$AnimationPlayer.play("run")
+	elif is_on_floor() and input_dir == Vector2.ZERO:
+		$AnimationPlayer.play("idle")
+		
+	# Rotate the camera left / right
+	if Input.is_action_just_pressed("cam_left"):
+		$Camera_Controller.rotate_y(deg_to_rad(30))
+	if Input.is_action_just_pressed("cam_right"):
+		$Camera_Controller.rotate_y(deg_to_rad(-30))
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,11 +31,24 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# New Vector3 direction, taking into account the user arrow inputs and the camera rotation
+	var direction = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Rotate character mesh so oriented in the direction moving in relation to the camera
+	if input_dir != Vector2(0, 0):
+		$Armature.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) + -90
+	
+	# Rotate the character to align with the floor
+	if is_on_floor() and input_dir != Vector2(0, 0):
+		align_with_floor($RayCast3D.get_collision_normal())
+		global_transform = global_transform.interpolate_with(xform, 0.3)
+	elif not is_on_floor():
+		align_with_floor(Vector3.UP)
+		global_transform = global_transform.interpolate_with(xform, 0.3)
+		
+	
+	# Update the velocity and move the character
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,3 +57,18 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+	# Make Camera Controller Match the position of myself
+	$Camera_Controller.position = lerp($Camera_Controller.position, position, 0.15)
+
+func align_with_floor(floor_normal):
+	xform = global_transform
+	xform.basis.y = floor_normal
+	xform.basis.x = -xform.basis.z.cross(floor_normal)
+	xform.basis = xform.basis.orthonormalized()
+
+func _on_fall_zone_body_entered(body: Node3D) -> void:
+	get_tree().change_scene_to_file("res://game_over.tscn")
+	
+func bounce():
+	velocity.y = JUMP_VELOCITY
